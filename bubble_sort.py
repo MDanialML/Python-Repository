fd8f136c8f7962288d0d0d1f740237288895b774
M Danial <m.danial465@gmail.com>
2024-02-12 17:51:10 +0500

    sorting an array using bubble sort Bubble sort >> Optimized

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,10 +2,14 @@ l1 = [3,5,6,4,9,8,7]
 lentght = len(l1)
 
 for i in range(lentght-1):
+    swapped = False
     for j in range(lentght-i-1):
         if(l1[j] > l1[j+1]):
             temp = l1[j]
             l1[j] = l1[j+1]
             l1[j+1] = temp
+            swapped = True
+    if not swapped:
+        break
 
 print(l1)
